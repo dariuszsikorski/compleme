a7420800e1dcd91155d448fca67ecc70b7d4526f
Dariusz Sikorski <dariuszsikorski.pl@gmail.com>
2025-04-02 01:16:02 +0200

    feat: add sass

--- src/home-page/main.ts
+++ src/home-page/main.ts
@@ -1,14 +1,17 @@
-import './style.css'
+import './home.scss'
 import compleme from './compleme.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div class="home-page">
     <a href="/" target="_blank">
-      <img src="${compleme}" class="compleme logo" alt="Compleme logo" />
+      <img src="${compleme}" class="home-page_logo" alt="Compleme logo" />
     </a>
-    <h1>Compleme</h1>
-    <h2>A complementary set of utils for web app &amp; ui development</h2>
+    <h1 class="home-page_title">Compleme</h1>
+    <h2 class="home-page_subtitle">A complementary set of utils for web app &amp; ui development</h2>
+    <div class="home-page_counter-container u-flex-center">
+      <button id="counter" class="home-page_counter" type="button">Count is 0</button>
+    </div>
   </div>
 `
 
